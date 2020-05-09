85d93c8cd0db779ff4a23241c342e5c56e919bd3
Justina <justina.ridz@gmail.com>
2020-05-10 00:25:07 +0300

    Array madness

---
+++ js/madness.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function arrayMadness(a, b) {
+    let kvadratu = 0;
+    let kubu = 0;
+    for (let i=0; i<a.length; i++){
+        kvadratu += a[i]**2;
+    }
+    for (let i=0; i<b.length; i++){
+        kubu += b[i]**3;
+    }
+    if (kvadratu > kubu){
+        return true;
+    } else{
+        return false;
+    }
+  }
+
+
+
+
+
+console.log(arrayMadness([4,5,6],[1,2,3]), true);
+console.log(arrayMadness([5,6,7],[4,5,6]), false);
+console.log(arrayMadness([4,5,6],[3,4,5]), false);
+console.log(arrayMadness([3,4,5],[2,3,4]), false);
+console.log(arrayMadness([2,3,4],[1,2,3]), false);
+console.log(arrayMadness([1,2,3],[0,1,2]), true);
+console.log(arrayMadness([5,3,2,4,1],[15]), false);
+console.log(arrayMadness([2,5,3,4,1],[3,3,3,3,3]), false);
+console.log(arrayMadness([1,3,5,2,4],[2,2,2,2,2,2,2,1]), false);
+console.log(arrayMadness([1,2,3,4,5],[2,2,2,2,2,2,1,1,1]), true);
+console.log(arrayMadness([2,4,6,8,10,12,14],[1,3,5,7,9,11,13]), false)
