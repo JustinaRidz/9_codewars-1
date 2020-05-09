5295b61b83c809b24ab6134c48a02f2a5e67a135
Justina <justina.ridz@gmail.com>
2020-05-09 22:58:45 +0300

    Zero fuel

---
+++ js/zerofuel.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    if ( fuelLeft * mpg >= distanceToPump) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(zeroFuel(50, 25, 2), true);
+console.log(zeroFuel(100, 50, 1), false);
